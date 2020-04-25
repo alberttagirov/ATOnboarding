d144eaf4eb7b7a3ee86e00cec0a5c42644f046ff
Albert Tagirov <head@naranmail.ru>
2020-04-25 14:31:25 +0300

    Initial Commit

---
+++ ATOnboarding/ATOnboarding.h
@@ -0,0 +1,19 @@
+//
+//  ATOnboarding.h
+//  ATOnboarding
+//
+//  Created by Albert Tagirov on 25.04.2020.
+//  Copyright © 2020 Albert Tagirov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ATOnboarding.
+FOUNDATION_EXPORT double ATOnboardingVersionNumber;
+
+//! Project version string for ATOnboarding.
+FOUNDATION_EXPORT const unsigned char ATOnboardingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ATOnboarding/PublicHeader.h>
+
+
